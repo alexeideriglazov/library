139c2dbc753bd5913180efcb8f5858a4fe0dedc2
alexeideriglazov <alexeideriglazov@gmail.com>
2020-08-18 17:33:43 +0300

    Entity classes

---
+++ src/main/java/com/spring/library/domain/Publisher.java
@@ -0,0 +1,36 @@
+package com.spring.library.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+import javax.persistence.*;
+import java.util.List;
+
+@EqualsAndHashCode(of = "id")
+@Table(catalog = "library")
+@Getter
+@Setter
+@DynamicUpdate
+@DynamicInsert
+@SelectBeforeUpdate
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "publisher", fetch = FetchType.LAZY)
+    private List<Book> books;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
