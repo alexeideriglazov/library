d27f7ffff7b6055aaae74ea58a234606259c0c24
alexeideriglazov <alexeideriglazov@gmail.com>
2020-08-26 13:09:20 +0300

    Edit dao

--- src/main/java/com/spring/library/dao/BookDao.java
+++ src/main/java/com/spring/library/dao/BookDao.java
@@ -1,9 +1,15 @@
 package com.spring.library.dao;
 
 import com.spring.library.domain.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
 public interface BookDao extends GeneralDAO<Book> {
     List<Book> findTopBooks(int limit);
+
+    byte[] getContent(int id);
+
+    Page<Book> findByGenre(int pageNumber, int pageSize, String sortField, Sort.Direction sortDirection, int genreId);
 }
